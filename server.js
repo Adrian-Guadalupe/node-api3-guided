5aebba2bbf020b1873f5ce3af37551522f06bd1a
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-03-17 00:27:21 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-const express = require('express'); // importing a CommonJS module
+const express = require('express') // importing a CommonJS module
+const helmet = require('helmet')
+const morgan = require('morgan')
 
-const hubsRouter = require('./hubs/hubs-router.js');
+const hubsRouter = require('./hubs/hubs-router.js')
 
-const server = express();
+const app = express()
 
-server.use(express.json());
 
-server.use('/api/hubs', hubsRouter);
+// built in middleware
+app.use(express.json())
 
-server.get('/', (req, res) => {
-  const nameInsert = (req.name) ? ` ${req.name}` : '';
+// 3rd part middleware
+app.use(helmet())
+
+// custom middleware
+app.use(methodLogger)
+app.use(addName)
+app.use(lockout)
+
+app.use('/api/hubs', hubsRouter)
+
+app.get('/', (req, res) => {
+  const nameInsert = (req.name) ? ` ${req.name}` : ''
 
   res.send(`
     <h2>Lambda Hubs API</h2>
@@ -17,4 +29,29 @@ server.get('/', (req, res) => {
     `);
 });
 
-module.exports = server;
+function methodLogger(req, res, next) {
+  console.log(`${req.method} Request`)
+  next()
+}
+
+function addName(req, res, next) {
+  req.name = req.name || 'Rufus'
+  next()
+}
+
+// function lockout(req, res, next) {
+//   res.status(403).json({ message: 'api lockout in force' })
+// }
+
+function lockout(req, res, next) {
+  const d = new Date().getSeconds()
+  (d % 2 !== 0)
+    ? res.status(403).send('You shall not pass!')
+    : next()
+}
+
+app.use((error, req, res, next) => {
+  res.status(400).json({ message: 'bad panda!', error })
+})
+
+module.exports = app
